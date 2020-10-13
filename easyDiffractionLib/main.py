fa00a9338df6ea24c1c34fa01b879ef240f95e97
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2020-10-13 14:40:11 +0200

    Add test action

--- easyDiffractionLib/main.py
+++ easyDiffractionLib/main.py
@@ -1,15 +1,22 @@
 import os, sys
 
-import CFML_api
-import matplotlib.pyplot as plt
+#import CFML_api
 
 
 def main():
-    print("AAA")
+    print("without CFML_api")
+    exit()
+
+    simulation_conditions = CFML_api.PowderPatternSimulationConditions()
+    print("simulation_conditions.theta_max", simulation_conditions.theta_max)
+
     powder_pattern = CFML_api.PowderPatternSimulator()
+    print("powder_pattern.x", powder_pattern.x)
+    print("powder_pattern.y", powder_pattern.y)
+
     powder_pattern.compute("../CFML_api/Examples/Data/SrTiO3.cif")
-    plt.plot(powder_pattern.x, powder_pattern.y, label="CIF with default conditions")
-    plt.show()
+    print("powder_pattern.x", powder_pattern.x)
+    print("powder_pattern.y", powder_pattern.y)
 
 if __name__ == '__main__':
     main()

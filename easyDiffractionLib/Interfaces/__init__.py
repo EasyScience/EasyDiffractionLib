04c3107a5b2064261831db5cadef8186b76bf550
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2020-10-26 16:46:18 +0100

    Initial implementation of GSAS-II

--- easyDiffractionLib/Interfaces/__init__.py
+++ easyDiffractionLib/Interfaces/__init__.py
@@ -1,6 +1,9 @@
 __author__ = 'github.com/wardsimon'
 __version__ = '0.0.1'
 
+
+import os, sys
+
 try:
     from easyDiffractionLib.Interfaces.CFML import CFML  # noqa: F401
 except ImportError:
@@ -11,6 +14,11 @@ try:
 except ImportError:
     # TODO make this a proper message (use logging?)
     print('Cryspy is not installed')
-
+try:
+    #sys.path.insert(0, os.path.expanduser("~/gsas2full/GSASII/"))
+    from easyDiffractionLib.Interfaces.GSASII import GSASII  # noqa: F401
+except ImportError:
+    # TODO make this a proper message (use logging?)
+    print('GSASII is not installed')
 
 from easyDiffractionLib.Interfaces.interfaceTemplate import InterfaceTemplate

6a300948e7c6a473862eeeb05c0698fed1603ee4
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2020-10-27 22:53:41 +0100

    Update import GSASII

--- easyDiffractionLib/Interfaces/__init__.py
+++ easyDiffractionLib/Interfaces/__init__.py
@@ -14,14 +14,23 @@ try:
     from easyDiffractionLib.Interfaces.cryspy import Cryspy  # noqa: F401
 except ImportError:
     # TODO make this a proper message (use logging?)
-    print('Cryspy is not installed')
+    print('CrysPy is not installed')
 
 try:
     import GSASII
     sys.path.insert(0, GSASII.__path__[0])
+    if 'darwin' in platform:
+        import somacos
+        sys.path.insert(0, os.path.join(somacos.__path__[0], "GSASII"))
+    elif 'linux' in platform:
+        import solinux
+        sys.path.insert(0, os.path.join(solinux.__path__[0], "GSASII"))
+    elif 'win32' in platform:
+        import sowindows
+        sys.path.insert(0, os.path.join(sowindows.__path__[0], "GSASII"))
     from easyDiffractionLib.Interfaces.GSASII import GSASII  # noqa: F401
 except ImportError:
     # TODO make this a proper message (use logging?)
-    print('GSASII is not installed')
+    print('GSAS-II is not installed')
 
 from easyDiffractionLib.Interfaces.interfaceTemplate import InterfaceTemplate

06d9e570dd4046510f843a3833125c78de4806a3
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2020-10-27 18:08:36 +0100

    Import GSASII

--- easyDiffractionLib/Interfaces/__init__.py
+++ easyDiffractionLib/Interfaces/__init__.py
@@ -9,13 +9,16 @@ try:
 except ImportError:
     # TODO make this a proper message (use logging?)
     print('CFML is not installed')
+
 try:
     from easyDiffractionLib.Interfaces.cryspy import Cryspy  # noqa: F401
 except ImportError:
     # TODO make this a proper message (use logging?)
     print('Cryspy is not installed')
+
 try:
-    #sys.path.insert(0, os.path.expanduser("~/gsas2full/GSASII/"))
+    import GSASII
+    sys.path.insert(0, GSASII.__path__[0])
     from easyDiffractionLib.Interfaces.GSASII import GSASII  # noqa: F401
 except ImportError:
     # TODO make this a proper message (use logging?)

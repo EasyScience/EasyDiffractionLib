3bae5bbe9887613da07f08722b360889fdd0f35c
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2020-11-06 01:22:04 +0100

    Update __init__.py

--- easyDiffractionLib/Interfaces/__init__.py
+++ easyDiffractionLib/Interfaces/__init__.py
@@ -3,34 +3,38 @@ __version__ = '0.0.1'
 
 
 import os, sys
+import traceback
+
 
 try:
     from easyDiffractionLib.Interfaces.cryspy import Cryspy  # noqa: F401
 except ImportError:
     # TODO make this a proper message (use logging?)
-    print('CrysPy is not installed')
+    print('Warning: CrysPy is not installed')
 
 try:
-    from easyDiffractionLib.Interfaces.CFML import CFML  # noqa: F401
-except (ImportError, Exception):
-    # TODO make this a proper message (use logging?)
-    print('CFML is not installed')
-
+    from easyDiffractionLib.Interfaces.CFML import CFML
+except Exception:
+    traceback.print_exc()
+    print('Warning: CFML is not installed')
+    
 try:
     import GSASII
-    sys.path.insert(0, GSASII.__path__[0])
+    gsasii_path = list(GSASII.__path__)[0]
+    sys.path.insert(0, gsasii_path)
     if 'darwin' in sys.platform:
         import libsDarwin
-        sys.path.insert(0, os.path.join(libsDarwin.__path__[0], "GSASII"))
+        libs_path = list(libsDarwin.__path__)[0]
     elif 'linux' in sys.platform:
         import libsLinux
-        sys.path.insert(0, os.path.join(libsLinux.__path__[0], "GSASII"))
+        libs_path = list(libsLinux.__path__)[0]
     elif 'win32' in sys.platform:
         import libsWin32
-        sys.path.insert(0, os.path.join(libsWin32.__path__[0], "GSASII"))
+        libs_path = list(libsWin32.__path__)[0]
+    sys.path.insert(0, os.path.join(libs_path, "GSASII"))
     from easyDiffractionLib.Interfaces.GSASII import GSASII  # noqa: F401
-except (ImportError, Exception):
-    # TODO make this a proper message (use logging?)
-    print('GSAS-II is not installed')
+except Exception:
+    traceback.print_exc()    
+    print('Warning: GSAS-II is not installed')
 
 from easyDiffractionLib.Interfaces.interfaceTemplate import InterfaceTemplate

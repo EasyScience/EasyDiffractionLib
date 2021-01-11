4c44a735ade68495aa890302f41c4dd29936bc7b
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2021-01-11 10:53:22 +0100

    Use new CFML api

--- easyDiffractionLib/main.py
+++ easyDiffractionLib/main.py
@@ -1,21 +1,10 @@
 import os, sys
 
-from CFML_api import PowderPatternSimulation as CFML_api
+from easyDiffractionLib.interface import InterfaceFactory
 
 def main():
-    print("without CFML_api")
-    # exit()
-
-    simulation_conditions = CFML_api.PowderPatternSimulationConditions()
-    print("simulation_conditions.theta_max", simulation_conditions.theta_max)
-
-    powder_pattern = CFML_api.PowderPatternSimulator()
-    print("powder_pattern.x", powder_pattern.x)
-    print("powder_pattern.y", powder_pattern.y)
-
-    # powder_pattern.compute("../CFML_api/Examples/Data/SrTiO3.cif")
-    # print("powder_pattern.x", powder_pattern.x)
-    # print("powder_pattern.y", powder_pattern.y)
+    interface = InterfaceFactory()
+    print(f"Available interfaces: {interface.available_interfaces}")
 
 if __name__ == '__main__':
     main()

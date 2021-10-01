fb0e90395fbd4712b012d25b53a4b2c4f7baf069
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2021-10-01 15:38:02 +0200

    Update generate_html.py

--- tools/Scripts/generate_html.py
+++ tools/Scripts/generate_html.py
@@ -4,7 +4,7 @@ __version__ = "0.0.1"
 import os
 
 whl_dir = "dist"
-base_url = "https://github.com/easyScience/easyDifractionLib/releases/download/develop/"
+base_url = "https://github.com/easyScience/easyDifractionLib/releases/download/eDL_alpha/"
 
 file = [file for file in os.listdir(whl_dir) if file.endswith(".whl")][0]
 

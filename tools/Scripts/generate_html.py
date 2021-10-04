4f33ee51683941ab2b14df1a40f7cb07dde06f68
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2021-10-04 14:49:07 +0200

    Write all packages

--- tools/Scripts/generate_html.py
+++ tools/Scripts/generate_html.py
@@ -1,20 +1,26 @@
 __author__ = "github.com/wardsimon"
 __version__ = "0.0.1"
 
-import os
 import sys
+import requests
 
-tag = "eDL_alpha"
+org = "easyScience"
+repo = "easyCore"
 if len(sys.argv) > 1:
-    tag = sys.argv[1]
+    repo = sys.argv[1]
 
-whl_dir = "dist"
-base_url = f"https://github.com/easyScience/easyDiffractionLib/releases/download/{tag}/"
+releases = requests.get(f"https://api.github.com/repos/{org}/{repo}/releases").json()
 
-file = [file for file in os.listdir(whl_dir) if file.endswith(".whl")][0]
-
-header = "<!DOCTYPE html>\n<html>\n<head>\n<title>Links for easyDiffractionLib (alpha)</title>\n</head>\n<body>\n<h1>Links for easyDiffractionLib</h1>"
-body = f'<a href="{base_url}{file}" data-requires-python="&gt=3.7,&lt4.0">{file[:-4]}</a><br/>'
+header = f"<!DOCTYPE html>\n<html>\n<head>\n<title>Links for {repo} (alpha)</title>\n</head>\n<body>\n<h1>Links for {repo}</h1>"
+body = ""
+for release in releases:
+    asset_url = release["assets_url"]
+    assets = requests.get(asset_url).json()
+    for asset in assets:
+        if asset["name"].endswith(".whl"):
+            name = asset["name"][:-4]
+            url = asset["browser_download_url"]
+            body += f'<a href="{url}" data-requires-python="&gt=3.7,&lt4.0">{name}</a><br/>\n'
 footer = "</body>\n</html>"
 
 content = "\n".join([header, body, footer])

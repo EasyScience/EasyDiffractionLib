2942d816cc9b124f98de7281ed62915fa554333e
Piotr Rozyczko <piotr.rozyczko@gmail.com>
2022-04-11 14:42:14 +0200

    added msp support

--- easyDiffractionLib/components/site.py
+++ easyDiffractionLib/components/site.py
@@ -39,11 +39,12 @@ class Site(ecSite):
         fract_y: Optional[Union[float, Parameter]] = None,
         fract_z: Optional[Union[float, Parameter]] = None,
         adp: Optional[Union[str, AtomicDisplacement]] = None,
+        msp: Optional[Union[str, MagneticSusceptibility]] = None,
         interface: Optional[iF] = None,
         **kwargs,
     ):
 
-        if "msp" in kwargs.keys():
+        if msp is not None:
             msp = kwargs.pop("msp")
             if isinstance(msp, str):
                 msp = MagneticSusceptibility(msp)
@@ -80,6 +81,8 @@ class PeriodicSite(ecPeriodicSite):
         kwargs = ecPeriodicSite._from_site_kwargs(lattice, site)
         if hasattr(site, "adp"):
             kwargs["adp"] = site.adp
+        if hasattr(site, "msp"):
+            kwargs["msp"] = site.msp
         return cls(**kwargs)
 
 
@@ -88,7 +91,18 @@ class Atoms(ecAtoms):
     _SITE_CLASS = Site
 
     def to_star(self) -> List[StarLoop]:
+        add_loops = []
         main_loop = super(Atoms, self).to_star()[0]
+
+        self.add_adp(main_loop, add_loops)
+        self.add_msp(main_loop, add_loops)
+
+        loops = [main_loop, *add_loops]
+
+        return loops
+
+    def add_adp(self, main_loop, add_loops):
+
         adps = [hasattr(item, "adp") for item in self]
         has_adp = any(adps)
         if not has_adp:
@@ -110,9 +124,42 @@ class Atoms(ecAtoms):
                 add_loops.append(StarLoop.from_StarSections(entries))
         else:
             raise NotImplementedError("Multiple types of ADP are not supported")
-        loops = [main_loop, *add_loops]
-        return loops
+        return add_loops
 
+    def add_msp(self, main_loop, add_loops):
 
+        msps = [hasattr(item, "msp") for item in self]
+        has_msp = any(msps)
+        if not has_msp:
+            # initialize msp so as_dict doesn't throw a fit
+            for item in self:
+                msp = MagneticSusceptibility("Cani")
+                item.msp = msp
+        add_loops = []
+        msp_types = [item.msp.msp_type.raw_value for item in self if hasattr(item, 'msp')]
+        if all(msp_types):
+            if msp_types[0] in ["Cani", "Ciso"]:
+                loops = []
+                for item in self:
+                    if not hasattr(item, 'msp'):
+                        msp_item = MagneticSusceptibility(msp_types[0])
+                        item.msp = msp_item
+                    loops.append(getattr(item, 'msp').to_star(item.label))
+                msp_loop = StarLoop.from_StarSections(loops)
+                main_loop = main_loop.join(msp_loop, "label")
+            else:
+                pass
+                entries = []
+                for item in self:
+                    if hasattr(item, 'msp'):
+                        entries.append(item.msp.to_star(item.label))
+                    else:
+                        msp = MagneticSusceptibility(msp_types[0])
+                        item.msp = msp
+                        entries.append(msp.to_star(item.label))
+                add_loops.append(StarLoop.from_StarSections(entries))
+        else:
+            raise NotImplementedError("Multiple types of MSP are not supported")
+        return add_loops
 class PeriodicAtoms(ecPeriodicAtoms):
     _SITE_CLASS = PeriodicSite

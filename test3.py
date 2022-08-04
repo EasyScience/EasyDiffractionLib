84011a1a00074b75d909f87a9f4dfadcfca68dcb
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2022-08-04 13:40:37 +0200

    Delete test3.py

--- test3.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'github.com/wardsimon'
-__version__ = '0.0.1'
-
-from numpy import pi
-from easyCrystallography.Components.Site import Site
-from easyCrystallography.Components.Specie import Specie
-
-s_ = Site(specie="Si")

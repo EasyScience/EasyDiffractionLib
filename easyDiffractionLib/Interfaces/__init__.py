10e15d4472e4c3caedc7f7415bb94ee134fc88f4
Piotr Rozyczko <piotr.rozyczko@gmail.com>
2023-10-12 17:46:28 +0200

    temporarily remove crysfml dependency

--- easyDiffractionLib/Interfaces/__init__.py
+++ easyDiffractionLib/Interfaces/__init__.py
@@ -19,11 +19,11 @@ except ImportError:
     # TODO make this a proper message (use logging?)
     print('Warning: CrysPy is not installed')
 
-try:
-    from easyDiffractionLib.Interfaces.CFML import CFML  # noqa: F401
-except ImportError:
-    # TODO make this a proper message (use logging?)
-    print('Warning: CrysFML is not installed')
+# try:
+#     from easyDiffractionLib.Interfaces.CFML import CFML  # noqa: F401
+# except ImportError:
+#     # TODO make this a proper message (use logging?)
+#     print('Warning: CrysFML is not installed')
 
 # try:
 #     from easyDiffractionLib.Interfaces.GSASII import GSASII  # noqa: F401

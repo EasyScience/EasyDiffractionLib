08b38d1194ce01f88d94db53414761a056a547c5
Andrew Sazonov <andrew.sazonov@icloud.com>
2024-11-08 09:54:08 +0100

    Update examples

--- easydiffraction/__init__.py
+++ easydiffraction/__init__.py
@@ -9,7 +9,7 @@ from .components.phase import Phase as Phase
 from .components.phase import Phases as Phases
 from .components.site import Atoms as Atoms
 from .components.site import Site as Site
-from .Job import DiffractionJob as Job
 from .io.download import download_from_repository
+from .Job import DiffractionJob as Job
 
 __all__ = ['Job']

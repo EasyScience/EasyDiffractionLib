2327658971580f62336008bce1987812a43582e5
Andrew Sazonov <andrew.sazonov@icloud.com>
2024-11-26 16:01:55 +0100

    Add more aliases for easier usage

--- src/easydiffraction/__init__.py
+++ src/easydiffraction/__init__.py
@@ -2,7 +2,10 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # © 2021-2024 Contributors to the EasyDiffraction project <https://github.com/EasyScience/EasyDiffraction>
 
+# Aliases for the main classes and methods to simplify the usage
 from .job.job import DiffractionJob as Job
+from .job.model.phase import Phase
 from .utils import download_from_repository
 
-__all__ = ['Job', 'download_from_repository']
+# Globals, which are imported from easydiffraction by a star import
+__all__ = ['Job', 'Phase', 'download_from_repository']

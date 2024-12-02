5ddc59c8d5d5a2b875606e53616f826234639108
Andrew Sazonov <andrew.sazonov@icloud.com>
2024-12-02 12:51:42 +0100

    Add new init files for project and summary modules

---
+++ src/easydiffraction/job/summary/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024 EasyDiffraction contributors
+# SPDX-License-Identifier: BSD-3-Clause
+# © 2021-2024 Contributors to the EasyDiffraction project <https://github.com/EasyScience/EasyDiffraction>

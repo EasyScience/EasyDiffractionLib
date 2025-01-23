6c76b7fb196e9801df7974459e7555e6d4b03e4a
Andrew Sazonov <andrew.sazonov@icloud.com>
2025-01-23 13:36:02 +0100

    Update linting, formatting, and testing scripts
    - Added `--fix` option to notebook formatting check
    - Introduced a new script for running Jupyter notebooks
    - Included functional tests in the test runner script

--- tools/linting_and_formatting.sh
+++ tools/linting_and_formatting.sh
@@ -5,7 +5,7 @@ echo "\033[0;33m:::::: Check and fix code formatting\033[0m"
 ruff format .
 
 echo "\033[0;33m:::::: Check and fix notebook formatting\033[0m"
-nbqa ruff examples/
+nbqa ruff examples/ --fix
 
 echo "\033[0;33m:::::: Check and fix non-code formatting\033[0m"
 npx prettier . --write --config=prettierrc.toml
